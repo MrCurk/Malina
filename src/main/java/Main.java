d013dd6552d483c89f3d50a458c69a60848a3f6c
Mr.Curk <mr.curk@gmail.com>
2015-12-29 23:48:15 +0100

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by one on 29.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Malina started!");
+    }
+}
---
+++ src/main/java/mr/curk/piface/PiFaceLogic.java
@@ -0,0 +1,7 @@
+package mr.curk.piface;
+
+/**
+ * Created by one on 29.12.2015.
+ */
+public class PiFaceLogic {
+}
